e3f6a3ab98283e7b5c44b6705cb93e10c1352c3d
Andrija Skontra <askontra22@student.unizd.hr>
2024-07-22 13:55:21 +0200

    add header and basic navigation

---
+++ sync-snack/app/(main)/layout.tsx
@@ -0,0 +1,20 @@
+import Header from "../components/Header";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <div>
+      <header className="fixed top-0 left-0 right-0 z-50">
+        <Header />
+      </header>
+      <div className="mt-24">
+        {children}
+      </div>
+    </div>
+  );
+}
+
