1ffc1f1cd35ed9874784cc9fe1179dcac395b23d
Andrija Skontra <askontra22@student.unizd.hr>
2024-07-22 12:25:49 +0200

    add chakra and remove nextjs template page

---
+++ sync-snack/app/providers.tsx
@@ -0,0 +1,6 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <ChakraProvider>{children}</ChakraProvider>
+}
+
