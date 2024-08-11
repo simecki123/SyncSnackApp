6bf2b2285bfb99637bf340eb238b65b25c55ed1e
AndrijaSkontra <askontra22@student.unizd.hr>
2024-08-11 13:22:03 +0200

    area52 50%

--- sync-snack/app/area52/page.tsx
+++ sync-snack/app/area52/page.tsx
@@ -1,11 +1,16 @@
 'use client'
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Text, Input, Textarea } from "@chakra-ui/react";
 import { Client, Message } from '@stomp/stompjs';
+import clsx from "clsx";
 import { useEffect, useState } from "react";
 
 export default function Area52Page() {
 
   const [connection, setConnection] = useState('NO CONNECTION')
+
+  const [inputValue, setInputValue] = useState('');
+
+  const [messages, setMessages] = useState<any[]>([])
 
   const client = new Client();
   client.brokerURL = 'ws://localhost:8080/ws';
@@ -14,22 +19,59 @@ export default function Area52Page() {
 
     client.onConnect = function(frame) {
       console.log('CONNECTED TO WEB SOCKET')
-      setConnection('CONNECTED')
+      if (client.connected) {
+        setConnection('CONNECTED')
+      } else (
+        setConnection('CAN\'T CONNECT')
+      )
     }
   }, [])
+
+  const handleInputChange = (e: any) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    console.log(inputValue);
+    const messagesCopy = [...messages]
+    messagesCopy.push(inputValue)
+    setMessages(messagesCopy)
+  };
 
   return (
     <Box className="h-full w-full flex flex-col space-y-10 items-center">
       <Text className="text-4xl font-semibold">WELCOME TO WEB SOCKET TESTING GROUND</Text>
       <Text>{connection}</Text>
       <Button onClick={() => {
-        setConnection('CONNECTED')
-        client.activate()
+        if (client.connected) {
+          setConnection('CONNECTED')
+          client.activate()
+        } else (
+          setConnection('CAN\'T CONNECT')
+        )
       }}>Connect</Button>
       <Button onClick={() => {
         setConnection('DISCONNECTED')
         client.deactivate()
       }}>Disconnect</Button>
+      <Box className="flex">
+        <Input
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter text here"
+          className="mb-4"
+        />
+        <Button
+          onClick={handleButtonClick}
+          className={clsx("border-blue-600 border-2", { "border-red-600": connection !== "CONNECTED" })}>Send</Button>
+      </Box>
+      <Box className="border-black border-2">
+        {messages.map((value, index) => {
+          return (
+            <Text key={index}>{value}</Text>
+          )
+        })}
+      </Box>
     </Box>
   )
 }
