baa200d2ddc1be8cfab449781e0e0c13b0ae874e
Šime Rončević <sime.roncevic@kodelab.hr>
2024-08-13 14:14:33 +0200

    Adding listener and updating home

---
+++ sync-snack/app/eventBus.ts
@@ -0,0 +1,14 @@
+const eventBus = {
+    on(event: string, callback: EventListenerOrEventListenerObject) {
+      document.addEventListener(event, callback);
+    },
+    dispatch(event: string, data?: any) {
+      document.dispatchEvent(new CustomEvent(event, { detail: data }));
+    },
+    remove(event: string, callback: EventListenerOrEventListenerObject) {
+      document.removeEventListener(event, callback);
+    },
+  };
+  
+  export default eventBus;
+  
