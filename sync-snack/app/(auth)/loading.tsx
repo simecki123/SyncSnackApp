b0ac02ae0503ae287acb2f9cac488c3a699d57dd
Andrija Skontra <askontra22@student.unizd.hr>
2024-08-22 09:55:57 +0200

    add loading

---
+++ sync-snack/app/(auth)/loading.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { Box } from "@chakra-ui/react";
+import dynamic from 'next/dynamic';
+
+const DotLottieReact = dynamic(
+  () => import('@lottiefiles/dotlottie-react').then((mod) => mod.DotLottieReact),
+  { ssr: false }
+);
+
+export default function Loading() {
+  return (
+    <Box className="h-screen w-screen flex items-center justify-center text-white">
+      <DotLottieReact
+        src="/loading.json"
+        loop
+        autoplay
+        className="w-72 h-72 m-44"
+      />
+    </Box>
+  )
+}
+
