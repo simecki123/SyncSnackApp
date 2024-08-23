7d9d629ffa916567e01d72fe70009d7b5ae1e03c
Andrija Skontra <askontra22@student.unizd.hr>
2024-08-23 15:57:21 +0200

    better header

--- sync-snack/app/components/header/HeaderLogo.tsx
+++ sync-snack/app/components/header/HeaderLogo.tsx
@@ -1,11 +1,14 @@
-import { Box, Link, Image } from "@chakra-ui/react";
+import { Box, Link, Image, Text } from "@chakra-ui/react";
 import SyncSnackLogo from "./SyncSnackLogo";
 
 export default function HeaderLogo() {
   return (
     <Link href="/home">
-      <Box className="bg-orange-100 rounded-xl m-4">
-        <Image alt="logo" src="/logo.png" boxSize={44} className="rounded-full" />
+      <Box className="bg-orange-100 rounded-xl m-4 py-2 w-full">
+        <Box className="flex justify-center ">
+          <Image alt="logo" src="/logo2.png" className="rounded-full h-20 w-44 pl-2" />
+        </Box>
+        <Text className="flex justify-center font-bold text-2xl text-orange-900">SyncSnack</Text>
       </Box>
     </Link>
   )
