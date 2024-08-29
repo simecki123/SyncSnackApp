ffcf2b56df69f56fd50defc903c39522053d938c
Šime Rončević <sime.roncevic@kodelab.hr>
2024-08-29 09:15:49 +0200

    Fixing mobile view of the tool bar

--- sync-snack/app/components/header/HeaderPhone.tsx
+++ sync-snack/app/components/header/HeaderPhone.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Box, Button, Text, useDisclosure } from "@chakra-ui/react"
-import { useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import HamburgerMenu from "./HamburgerMenu"
 import NavLinks from "./NavLinks"
 import SyncSnackLogo from "./SyncSnackLogo"
@@ -15,24 +15,44 @@ import {
   DrawerCloseButton,
 } from '@chakra-ui/react'
 import NotificationBell from "../notification/NotificationBell"
+import { SignOutButton } from "./SignOutButton"
+import { NavLinksContext } from "@/app/providers"
 
 export default function HeaderPhone({ activeUser }: any) {
 
   const [isOpenNav, setIsOpen] = useState(false)
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const eventContext = useContext(NavLinksContext)
 
 
-  function NavigationPhone() {
-    return (
-      <Box className="bg-orange-100 hover:scale-75 transition">
-        <NavLinks setIsOpen={setIsOpen} />
-      </Box>
-    )
-  }
+  
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/events/active`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${activeUser.accessToken}`
+      },
+    }).then((value) => {
+      if (!value.ok) {
+        throw new Error('no event')
+      }
+      eventContext.setIsEventLinkShown(true)
+      return value.json()
+    }).catch((_) => {
+      console.log('no current event')
+    });
+
+    if (activeUser.roles.includes('ADMIN')) {
+      eventContext.setIsAdminLinkShown(true)
+    }
+  })
+
+  
 
   return (
     <Box>
-      <Box className="flex items-center bg-orange-200 px-2 justify-between">
+      <Box className="flex items-center bg-blue-2 px-2 justify-between">
         <Box className="flex items-center">
           <SyncSnackLogo />
           <Text className="text-2xl font-semibold">SyncSnack</Text>
@@ -49,7 +69,8 @@ export default function HeaderPhone({ activeUser }: any) {
         <DrawerOverlay />
         <DrawerContent>
           <DrawerBody>
-            <NavLinks setIsOpen={onClose} />
+            <NavLinks setIsOpen={onClose} IsAdminLinkShown={eventContext.isAdminLinkShown} IsEventLinkShown={eventContext.isEventLinkShown} />
+            <SignOutButton />
           </DrawerBody>
         </DrawerContent>
       </Drawer>
